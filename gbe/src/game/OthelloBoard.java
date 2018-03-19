12f88f61819e474032f56d2be4760fb510793f47
Kenny <yaykennyjohn@gmail.com>
2018-03-18 19:48:54 -0700

    updated score UI

--- gbe/src/game/OthelloBoard.java
+++ gbe/src/game/OthelloBoard.java
@@ -4,6 +4,7 @@ import engine.Board;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,18 +14,55 @@ public class OthelloBoard implements Board {
     JFrame frame;
     OthelloState state;
     OthelloLogic logic;
+    JPanel game;
+    JPanel statusPanel;
+    JLabel status;
 
     public OthelloBoard() {
         state = new OthelloState();
         logic = new OthelloLogic(state);
+        frame = new JFrame("Othello");
+        frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
+        frame.setLocation(500, 100);
+        game = new JPanel(new GridLayout(8, 8));
+        game.setPreferredSize(new Dimension(600, 600));
+        frame.setLayout(new BorderLayout());
+        frame.setSize(300, 300);
+
+        statusPanel = new JPanel(); // container
+//        statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        statusPanel.setBackground(new Color(100, 72, 29));
+        statusPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        frame.add(statusPanel, BorderLayout.SOUTH);
+        statusPanel.setPreferredSize(new Dimension(frame.getWidth(), 60));
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+        status = new JLabel("<html>" + "<strong>Player 1:</strong> " + state.pm.score.get(state.pm.p1) + "<br/><strong>Player 2:</strong> " +
+                state.pm.score.get(state.pm.p2) + "</html>"); // value, here will be username and score,
+        status.setForeground(Color.white);
+        status.setFont(new Font("Arial", Font.PLAIN, 18));
+        status.setHorizontalAlignment(SwingConstants.CENTER);
+        statusPanel.add(status);
     }
 
     public void drawBoard(String gameType) {
-        frame = new JFrame(gameType);
-        frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
-        frame.setLocation(500, 100);
-        JPanel game = new JPanel(new GridLayout(8, 8));
-        game.setPreferredSize(new Dimension(600, 600));
+//        frame = new JFrame(gameType);
+//        frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
+//        frame.setLocation(500, 100);
+//        JPanel game = new JPanel(new GridLayout(8, 8));
+//        game.setPreferredSize(new Dimension(600, 600));
+//        frame.setLayout(new BorderLayout());
+//        frame.setSize(300, 300);
+
+        //creates the status bar panel and shoves it down the bottom of the frame
+//        statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+//        frame.add(statusPanel, BorderLayout.SOUTH);
+//        statusPanel.setPreferredSize(new Dimension(frame.getWidth(), 50));
+//        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+//        JLabel status = new JLabel("<html>" + "<strong>Player 1:</strong> " + state.pm.score.get(state.pm.p1) + "<br/><strong>Player 2:</strong> " +
+//                state.pm.score.get(state.pm.p2) + "</html>"); // value, here will be username and score,
+//        status.setFont(new Font("Serif", Font.PLAIN, 18));
+//        status.setHorizontalAlignment(SwingConstants.CENTER);
+//        statusPanel.add(status);
 
         try {
             UIManager.setLookAndFeel(new javax.swing.plaf.metal.MetalLookAndFeel());
@@ -87,6 +125,9 @@ public class OthelloBoard implements Board {
                     }
                 }
             }
+            // update label here
+            status.setText("<html>" + "<strong>Player 1:</strong> " + state.pm.score.get(state.pm.p1) + "<br/><strong>Player 2:</strong> " +
+                    state.pm.score.get(state.pm.p2) + "</html>");
         }
     }
 }
